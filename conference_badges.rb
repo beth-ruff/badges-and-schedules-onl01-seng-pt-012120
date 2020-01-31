43b39f4274bce9ea3053eed7a93783861cd8833d
Beth Ruff <beth.ruff2@gmail.com>
2020-01-31 01:55:32 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -13,8 +13,8 @@ def batch_badge_creator(attendees)
 end
 
 def conference_badges(attendees)
-  attendees.each_with_index(1).map do |attendee, index|
-    "Hello #{attendee}! You'll be assigned to room #{index}!"
+  attendees.each_with_index.map do |attendee, index|
+    "Hello #{attendee}! You'll be assigned to room #{index + 1}!"
   end
 end
   
