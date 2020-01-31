10036a625e7d63452ed8a569074b44e523f696f9
Beth Ruff <beth.ruff2@gmail.com>
2020-01-31 01:49:37 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -3,6 +3,7 @@ attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 def badge_maker(name)
  puts "Hello, my name is #{name}."
 end
+badge_maker("Arel")
 
 
 def batch_badge_creator(attendees)
